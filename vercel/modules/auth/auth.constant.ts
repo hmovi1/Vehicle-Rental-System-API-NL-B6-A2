d96b4dbbf1f7273cacaf3c4db200a341dd947e26
hmovi1 <hamidmuntasirovi1@gmail.com>
2026-01-17 19:10:48 +0600

    "vercel root fixed"

--- vercel/modules/auth/auth.constant.ts
+++
@@ -1,4 +0,0 @@
-export const Roles = {
-    admin: "admin",
-    customer: "customer"
-} as const;
--- vercel/modules/auth/auth.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { authController } from "./auth.controller"
-const route = Router()
-
-
-route.post("/signUp", authController.signUp)
-route.post("/signIn", authController.signIn)
-
-export const authRoute = route
