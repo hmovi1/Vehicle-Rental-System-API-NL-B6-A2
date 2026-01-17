2b542f27054e8d7784ca01e12e2cc4121f66d9a7
hmovi1 <hamidmuntasirovi1@gmail.com>
2026-01-17 17:43:37 +0600

    "config adjustment"

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
 // config.ts
 import dotenv from 'dotenv';
-dotenv.config();
 
 if (!process.env.CONNECTION_STRING) {
   throw new Error('connection string is not defined');
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
 }
