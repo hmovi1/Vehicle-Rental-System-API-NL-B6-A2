810ddfc5a8ba5053c8348803820b886d66d13dc6
hmovi1 <hamidmuntasirovi1@gmail.com>
2026-01-18 07:14:12 +0600

    "Fixed put vehicle role with admin-while testing"

--- src/modules/vehicles/vehicle.route.ts
+++ src/modules/vehicles/vehicle.route.ts
@@ -9,7 +9,7 @@ const router = Router();
 router.get('/', auth(Roles.admin, Roles.customer) , vehicleController.getAllVehicles);
 router.post('/', auth(Roles.admin) , vehicleController.createVehicle);
 router.get('/:vehicleId', auth(Roles.admin, Roles.customer),vehicleController.getVehicleById);
-router.put('/:vehicleId',  auth(Roles.customer), vehicleController.updateVehicle);
+router.put('/:vehicleId',  auth(Roles.admin), vehicleController.updateVehicle);
 router.delete("/:vehicleId", auth(Roles.admin), vehicleController.deleteVehicle);
 
 export const vehicleRoutes = router;
